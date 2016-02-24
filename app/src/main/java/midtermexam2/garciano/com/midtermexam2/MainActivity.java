e1eda86ff494928dcc9e94b8f7e42805e97403bc
jeanneviegarciano <jeanne_ctuie89@yahoo.com>
2016-02-23 19:28:12 -0800

    This is the implementation of getting data from http://joseniandroid.herokuapp.com/api/books

--- app/src/main/java/midtermexam2/garciano/com/midtermexam2/MainActivity.java
+++ app/src/main/java/midtermexam2/garciano/com/midtermexam2/MainActivity.java
@@ -3,12 +3,16 @@ package midtermexam2.garciano.com.midtermexam2;
 import android.app.ListActivity;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -17,42 +21,83 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends ListActivity {
 
+    private TextView booktitle;
+    public static String url = "http://joseniandroid.herokuapp.com/api/books";
 
-    private static String url = "http://172.17.3.51:5856/api/books";
+    public static final int SUCCESS_CODE = 200;
 
-    /*
-        private static final String TAG_CONTACTS = "contacts";
-        private static final String TAG_ID = "id";
-        private static final String TAG_NAME = "name";
-        private static final String TAG_EMAIL = "email";
-        private static final String TAG_ADDRESS = "address";
-        private static final String TAG_GENDER = "gender";
-        private static final String TAG_PHONE = "phone";
-        private static final String TAG_PHONE_MOBILE = "mobile";
-        private static final String TAG_PHONE_HOME = "home";
-        private static final String TAG_PHONE_OFFICE = "office";
-        */
+    public static final String PARAM_QUERY = "q";
+    public static final String PARAM_MODE = "mode";
+    public static final String PARAM_UNITS = "units";
+    public static final String PARAM_API_KEY = "appId";
+    private static final String OWM_CODE = "cod";
+
     private static final String TAG_BOOKS = "books";
     private static final String TAG_ID = "id";
     private static final String TAG_TITLE = "title";
     private static final String TAG_GENRE = "genre";
     private static final String TAG_AUTHOR = "author";
     private static final String TAG_ISREAD = "isread";
+    private static final String OWM_MAIN = "main";
 
-    JSONArray books = null;
+    public static Books getBooks(Uri uri, @NonNull String requestMethod) {
+        String json = HttpUtils.getResponse(uri, requestMethod);
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
 
-    ArrayList<HashMap<String, String>> bookList;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+        final String mid;
+        final String mtitle;
+        final String mgenre;
+        final String mauthor;
+        final boolean misread;
+
+
+        try {
+            JSONArray jsonObject = new JSONObject(json);
+            JSONArray jsonArray = jsonRootObject.optJSONArray("Books");
+
+
+            int statusCode = jsonArray.getJSONArray(OWM_CODE);
+            if (statusCode == SUCCESS_CODE) {
+            for(int i=0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+
+                mid = jsonArray.getJSONArray(TAG_ID);
+                mtitle = jsonArray.getJSONArray(TAG_TITLE);
+                mgenre = jsonArray.getJSONArray(TAG_GENRE);
+                mauthor = jsonArray.getJSONArray(TAG_AUTHOR);
+                misread = jsonArray.getJSONArray(TAG_ISREAD);
+
+
+                Books book = new Books()
+                        .setId(mid)
+                        .setTitle(mtitle)
+                        .setGenre(mgenre)
+                        .setAuthor(mauthor)
+                        .setIsread(misread);
+             }
+                return book;
+            } else {
+                return null;
+            }
+        } catch (
+                JSONException e
+                )
+
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
     @Override
@@ -61,85 +106,50 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
 
 
-        bookList = new ArrayList<HashMap<String, String>>();
-
         ListView lv = getListView();
 
+        booktitle = (TextView)findViewById(R.id.name);
 
-        new GetBooks().execute();
 
     }
 
 
-    private class GetBooks extends AsyncTask<Void, Void, Void> {
+    public class FetchWeatherTask extends AsyncTask<String, Void, Books> {
 
+        @Override
         protected void onPreExecute() {
 
         }
 
-        protected Void doInBackground(Void... arg0) {
-            // Creating service handler class instance
-            ServiceHandler sh = new ServiceHandler();
-
-            // Making a request to url and getting response
-            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-
-            Log.d("Response: ", "> " + jsonStr);
-
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-
-                    // Getting JSON Array node
-                    books = jsonObj.getJSONArray(TAG_BOOKS);
-
-                    // looping through All Contacts
-                    for (int i = 0; i < books.length(); i++) {
-                        JSONObject b = books.getJSONObject(i);
-
-                        String id = b.getString(TAG_ID);
-                        String title = b.getString(TAG_TITLE);
-                        String genre = b.getString(TAG_GENRE);
-                        String author = b.getString(TAG_AUTHOR);
-                        String isread = b.getString(TAG_ISREAD);
-
-
-                        // tmp hashmap for single contact
-                        HashMap<String, String> book = new HashMap<String, String>();
-
-                        // adding each child node to HashMap key => value
-                        book.put(TAG_ID, id);
-                        book.put(TAG_TITLE, title);
-                        book.put(TAG_GENRE, genre);
-                        book.put(TAG_AUTHOR, author);
-                        book.put(TAG_ISREAD, isread);
-
-                        // adding contact to contact list
-                        bookList.add(book);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Log.e("ServiceHandler", "Couldn't get any data from the url");
+        @Override
+        protected Books doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
             }
 
-            return null;
+
+
+            Uri builtUri = Uri.parse(MainActivity.url).buildUpon()
+                    .appendQueryParameter(MainActivity.PARAM_API_KEY,
+                            BuildConfig.OPEN_WEATHER_MAP_API_KEY)
+                    .build();
+
+            return MainActivity.getBooks(builtUri, "GET");
         }
 
-
         @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
+        protected void onPostExecute(Books b) {
+            if (b == null) {
 
-            ListAdapter adapter = new SimpleAdapter(
-                    MainActivity.this, bookList,
-                    R.layout.listview_item, new String[] { TAG_TITLE}, new int[] { R.id.name});
+                return;
 
-            setListAdapter(adapter);
+            }
+
+
+            booktitle.setText(String.format(b.getTitle()));
+
+
         }
 
     }
-
-
 }
